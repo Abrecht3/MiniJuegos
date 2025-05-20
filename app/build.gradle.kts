4ba843e2b061eb65ca716ed65bfc9c04392e4bcb
Abrecht3 <abrecht3@gmail.com>
2025-05-19 23:04:25 -0600

    1.0.3
    archivos remplazados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.albrecht3.minijuegos"
-        minSdk = 21
+        minSdk = 22
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -27,12 +27,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
+
     buildFeatures{
         viewBinding = true
     }
--- app/src/main/java/com/albrecht3/minijuegos/rockpaperscissors/GameRPSActivity.kt
+++ app/src/main/java/com/albrecht3/minijuegos/rockpaperscissors/GameRPSActivity.kt
@@ -3,14 +3,14 @@ package com.albrecht3.minijuegos.rockpaperscissors
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.albrecht3.minijuegos.R
 import com.albrecht3.minijuegos.databinding.ActivityGameRpsBinding
+import kotlinx.coroutines.delay
 
 class GameRPSActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameRpsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameRpsBinding.inflate(layoutInflater)
@@ -18,7 +18,44 @@ class GameRPSActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+            btnRock.setOnClickListener {
+                player1.setImageResource(R.drawable.rock)
+                jugadaCom(1)
+            }
+            btnPaper.setOnClickListener {
+                player1.setImageResource(R.drawable.paper)
+                jugadaCom(2)
+            }
+            btnScissor.setOnClickListener {
+                player1.setImageResource(R.drawable.scissors)
+                jugadaCom(3)
+            }
+        }
+    }
 
+    private fun jugadaCom(play: Int){
+        binding.apply {
+            var opcion: Int = ((Math.random()*3)+1).toInt()
+            when (opcion){
+                1->{
+                    playCPU.setImageResource(R.drawable.rock)
+                    if (play == 1) result.setText(R.string.draw)
+                    if (play == 2) result.setText(R.string.lose)
+                    if (play == 3) result.setText(R.string.win)
+                }
+                2->{
+                    playCPU.setImageResource(R.drawable.paper)
+                    if (play == 1) result.setText(R.string.win)
+                    if (play == 2) result.setText(R.string.draw)
+                    if (play == 3) result.setText(R.string.lose)
+                }
+                3->{
+                    playCPU.setImageResource(R.drawable.scissors)
+                    if (play == 1) result.setText(R.string.lose)
+                    if (play == 2) result.setText(R.string.win)
+                    if (play == 3) result.setText(R.string.draw)
+                }
+            }
         }
     }
 }
